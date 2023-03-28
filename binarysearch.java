174cedd9cb0a0fee0a903d57049e12b21b7e6c1a
KISHORE <91863645+kishore1912@users.noreply.github.com>
2023-03-28 09:17:45 +0530

    Update binarysearch.java

--- binarysearch.java
+++ binarysearch.java
@@ -1,8 +1,8 @@
 class main{
     public static void main(String args[])
     {
-        int arr[]={2,3,4,5,6,7,8,9};
-        int target=6;
+        int arr[]={9,8,7,4,3,2,1};
+        int target=8;
         System.out.println(Binarysearch(arr,target));
     }
     
@@ -10,21 +10,43 @@ class main{
     {
         int start=0;
         int end=arr.length-1;
+        
+        boolean isAsc=arr[start]<arr[end];
+        
+        
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(target<mid)
+            if(arr[mid]==target)
+        {
+            return mid;
+        }
+        
+        if(isAsc)
+        {
+            if(target<arr[mid])
             {
                 end=mid-1;
             }
-            else if(target>mid)
+            else
             {
                 start=mid+1;
             }
-            else
+            
+        }
+        else
+        {
+            if(target>arr[mid])
             {
-                return mid;
+                end=mid-1;
             }
+            else 
+            {
+                start=mid+1;
+            }
+            
+        }
+        
         }
         return -1;
     }
