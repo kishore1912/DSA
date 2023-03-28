6903594b71bc000ec5f3f6a2babf5bf9905cc7a4
KISHORE <91863645+kishore1912@users.noreply.github.com>
2023-03-28 22:20:46 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+class Bubblesort
+{
+    public static void main(String arge[])
+    {
+        int arr[]={8,7,1,9,5,0,3};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+    
+    static int[] sort(int[]arr)
+    {
+        for(int i=1;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
